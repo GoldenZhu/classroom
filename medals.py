219ba1da35e04b1d268d098228be3e4f79e8e767
GoldenZhu <2451255133@qq.com>
2017-10-31 21:37:05 +0800

    change func

--- medals.py
+++ medals.py
@@ -6,7 +6,7 @@ class Medals:
         for i in self.medal:
             if i == type:
                 self.medal[i] += 1
-                print 'The number of medals has increased %s'%i
+                print 'The %s number of medals has increased to %d'%(i, self.medal[i])
                 return
     def info(self):
         str = self.name + ': '
@@ -20,36 +20,30 @@ class Medals:
             sum += self.medal[j]
         print 'the total amount of %s medals is %d'%(self.name, sum)
         return sum
-
+# G for Golden, S for Silver, C for Copper.
 c = Medals('China', G=26, S=18, C=26)
 c.info()
 c.total()
+c.add('G')
 
 usa = Medals('USA', G=46, S=37, C=38)
-usa.info()
-usa.total()
-
 uk = Medals('UK', G=27, S=23, C=17)
-uk.info()
-uk.total()
-
 rus = Medals('RUS', G=19, S=18, C=19)
-rus.info()
-rus.total()
 
 def getObj(x):
     obj = {}
-    obj[x.name] = x.medal
-    return obj
+    obj['name'] = x.name
+    dictMerged = dict(obj, **x.medal)
+    return dictMerged
 
 obj1 = getObj(c)
 obj2 = getObj(usa)
 obj3 = getObj(uk)
 obj4 = getObj(rus)
-print obj1
-print obj2
-print obj3
-print obj4
+# print obj1
+# print obj2
+# print obj3
+# print obj4
 
 medalsList = []
 medalsList.append(obj1);
@@ -58,5 +52,11 @@ medalsList.append(obj3);
 medalsList.append(obj4);
 print medalsList
 
-# for i in medalsList:
-#     for k in medalsList.length - i:
+for i in range(len(medalsList)):
+    for k in range(len(medalsList)):
+        if k > i:
+            if medalsList[i]['G'] < medalsList[k]['G']:
+                obj = medalsList[i].copy()
+                medalsList[i] = medalsList[k].copy()
+                medalsList[k] = obj.copy()
+print medalsList
