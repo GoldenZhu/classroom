245dd87f6e244349015850ade05ebe418084bd3e
GoldenZhu <2451255133@qq.com>
2017-11-15 18:53:26 +0800

    add movie

---
+++ web-py/myWeb/code.py
@@ -0,0 +1,33 @@
+import web
+
+db = web.database(dbn='sqlite', db='MovieSite.db')
+render = web.template.render('templates/')
+
+urls = (
+	'/', 'index',
+	'/movie/(\d+)', 'movie',
+)
+
+class index:
+    def GET(self):
+        movies = db.select('movie')
+        return render.index(movies)
+
+	def POST(self):
+		data = web.input()
+		condition = r'title like "%' + data.title + r'%"'
+		print condition
+
+class movie:
+	def GET(self, movie_id):
+		movie_id = movie_id.encode('unicode-escape')
+		condition = 'id=' + movie_id
+		movie = db.select('movie', where=condition)[0]
+		return render.movie(movie)
+
+if __name__ == "__main__":
+	app = web.application(urls, globals())
+	app.run()
+
+
+
