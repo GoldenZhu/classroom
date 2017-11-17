26672b20477d3ae1d5aac9713302f1d3a6d1d834
GoldenZhu <2451255133@qq.com>
2017-11-17 17:00:39 +0800

    add search

--- web-py/myWeb/code.py
+++ web-py/myWeb/code.py
@@ -4,40 +4,58 @@ db = web.database(dbn='sqlite', db='MovieSite.db')
 render = web.template.render('templates/')
 
 urls = (
-	'/', 'index',
-	'/movie/(\d+)', 'movie',
-	'/cast/(.*)', 'cast',
+    '/', 'index',
+    '/movie/(\d+)', 'movie',
+    '/cast/(.*)', 'cast',
+    '/director/(.*)', 'director',
 )
+
 
 class index:
     def GET(self):
         movies = db.select('movie')
-        return render.index(movies)
+        count = db.query('SELECT COUNT(*) AS COUNT FROM movie')[0]['COUNT']
+        return render.index(movies, count, None)
 
     def POST(self):
-		data = web.input()
-		condition = r'title like "%' + data.title + r'%"'
-		movies = db.select('movie', where=condition)
-		return render.index(movies)
+        data = web.input()
+        condition = r'title like "%' + data.title + r'%"'
+        movies = db.select('movie', where=condition)
+        result = db.query('SELECT COUNT(*) AS COUNT FROM movie WHERE ' + condition)[0]
+        count = result['COUNT']
+        return render.index(movies, count, data.title)
 
 
 class movie:
-	def GET(self, movie_id):
-		movie_id = movie_id.encode('unicode-escape')
-		condition = "id='" + movie_id +"'"
-		movie = db.select('movie', where=condition)[0]
-		return render.movie(movie)
+    def GET(self, movie_id):
+        movie_id = movie_id.encode('unicode-escape')
+        condition = "id='" + movie_id +"'"
+        movie = db.select('movie', where=condition)[0]
+        return render.movie(movie)
+
 
 class cast:
-	def GET(self, cast_name):
-		condition = r'casts like "%' + cast_name + r'%"'
-		print condition
-		movies = db.select('movie', where=condition)
-		return render.index(movies)
+    def GET(self, cast_name):
+        condition = r'casts like "%' + cast_name + r'%"'
+        print condition
+        movies = db.select('movie', where=condition)
+        result = db.query('SELECT COUNT(*) AS COUNT FROM movie WHERE ' + condition)[0]
+        count = result['COUNT']
+        return render.index(movies, count, cast_name)
+
+
+class director:
+    def GET(self, director_name):
+        condition = r'directors like "%'+ director_name + r'%"'
+        print condition
+        movies = db.select('movie', where=condition)
+        result = db.query('SELECT COUNT(*) AS COUNT FROM movie WHERE ' + condition)[0]
+        count = result['COUNT']
+        print count
+        return render.index(movies, count, director_name)
 
 if __name__ == "__main__":
-	app = web.application(urls, globals())
-	app.run()
-
+    app = web.application(urls, globals())
+    app.run()
 
 
